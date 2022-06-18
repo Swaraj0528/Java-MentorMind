0ba6fafed4cc9d2de6def7c6da1448f095309da9
Swaraj Varma <swaraj.varma@outlook.com>
2022-06-18 10:19:35 +0530

    minor changes

--- src/main/java/com/massupdate/controller/Controller.java
+++ src/main/java/com/massupdate/controller/Controller.java
@@ -1,5 +1,14 @@
 package com.massupdate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
 
+	
+	@GetMapping
+	public String display() {
+		return "hi";
+	}
 }
