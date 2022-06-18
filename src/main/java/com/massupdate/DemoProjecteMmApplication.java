a131c4d996566bfbd44bd8e01cd504063cf0334a
Swaraj Varma <swaraj.varma@outlook.com>
2022-06-18 09:38:44 +0530

    initial commit

---
+++ src/main/java/com/massupdate/DemoProjecteMmApplication.java
@@ -0,0 +1,13 @@
+package com.massupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjecteMmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjecteMmApplication.class, args);
+	}
+
+}
